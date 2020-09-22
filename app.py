d28f6f52e264c80c6276e64b418d3ff6c5e0f797
Teale Foster <tealefoster@gmail.com>
2020-09-22 11:30:46 -0500

    import dependencies and build base for app

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
 
 # Create a New Flask App Instance
 app = Flask(__name__)
 
-# Create Flask Routes
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+
