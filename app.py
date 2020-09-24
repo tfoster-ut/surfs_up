8f41c768788aeeeb339e4dc45edd09dd73f528fd
Teale Foster <tealefoster@gmail.com>
2020-09-24 12:22:54 -0500

    add second route

--- app.py
+++ app.py
@@ -23,4 +23,30 @@ session = Session(engine)
 # Create a New Flask App Instance
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
+# @app.route('/api/v1.0/precipitation')
+# def precipitation():
+#     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days==365)
+#     precipitation = session.query(Measurement.date, Measurement.prcp).\
+#         filter(Measurement.date >= prev_year).all()
+#     precip = {date: prcp for date, prcp in precipitation}
+#     return jsonify(precip)
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
